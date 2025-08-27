9d392758079206100f8bdd1a50c1d64d2f825eb8
EaloEafit <lcealoo@eafit.edu.co>
2025-08-27 11:13:33 -0500

    Idle, Walking, Running Animations

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,6 +8,14 @@ public class InputManager : MonoBehaviour
     public float verticalInput;
     public float horizontalInput;
 
+    AnimatorManager animatorManager;
+    public float moveAmount;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
+
 
     private void OnEnable()
     {
@@ -28,6 +36,11 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
+
     }
 
     public void HandleAllInputs()
